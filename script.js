e6a25a4d8950d30e9226ba0ca3e3c2408ced3584
unknown <Eduarda Nunes>
2022-10-28 23:37:43 -0300

    exercicio5

---
+++ script.js
@@ -0,0 +1,10 @@
+var numero1 = 4;
+var numero2 = 5;
+
+if(numero1 == numero2){
+    console.log('os números são iguais!');
+}else if(numero1 > numero2){
+    console.log('o primeiro número é maior que o segundo número');
+}else{
+    console.log('o segundo número é maior que o primeiro!');
+}
